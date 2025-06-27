68d858075ee18ca9c2f6dc70e5b56856e56d21c6
Aman Gupta <gupta.aman2224@gmail.com>
2025-06-27 11:27:31 +0530

    Add files via upload

---
+++ Arrowfxn.js
@@ -0,0 +1,12 @@
+// --------PRACTICE arrow fxn----------
+
+const shoot = () =>{
+  alert("Goal hurray");
+}
+function Garbage(){
+  return(
+    <button onClick={shoot}>click me</button>
+  );
+}
+
+export default Garbage;
